a4f3a62463f448634b6b6eca5794b74eb519bf12
Hutton <r08922134@csie.ntu.edu.tw>
2022-02-24 21:42:40 +0800

    sync

--- backend/src/next-generation-sequencing/services/ngs.service.ts
+++ backend/src/next-generation-sequencing/services/ngs.service.ts
@@ -185,13 +185,15 @@ export class NGSService {
 	mergeFiles(files: Array<string>, bed:string, fileName:string): void{
 		const para1 = files.map((file)=>file+"_L001_R1_001.fastq.gz").join(' ');
 		const para2 = files.map((file)=>file+"_L001_R2_001.fastq.gz").join(' ');
-		var child = cp.execFile('bash',  [ `/home/pindel/code/merge.sh`,bed, para1,para2, fileName+"_S1"  ], 
+		var child = cp.execFileSync('bash',  [ `/home/pindel/code/merge.sh`,bed, para1,para2, fileName+"_S1"  ]);
+		/*
 			(error, stdout, stderr) => {
 				if (error) {
 				  throw error;
 				}
 				console.log(stdout);
-			});
+			}
+		*/
 	}
 	async getFilelist(): Promise<{}> {
 		var MPNstatus,TP53status,Myeloidstatus;
