562abe42eb039f7a5c1cd3b6b00505cfb1498d00
sean cheng <sfmech@yahoo.com>
2023-03-24 10:38:32 +0800

    Update ExportMPNPdf.tsx

--- frontend/src/components/ngs-result/ExportMPNPdf.tsx
+++ frontend/src/components/ngs-result/ExportMPNPdf.tsx
@@ -162,7 +162,7 @@ export const MyDocumentMPN: FunctionComponent<ExportPdfProps> = (prop) => (
 					<Text style={{ width: 175, fontFamily: 'KAIU' }}>檢體狀態 : {prop.data.specimenStatus}</Text>
 				</View>
 				<View style={styles.containInfoView}>
-					<Text style={{ width: 350, fontFamily: 'KAIU' }}>檢測項目 : MPN mutation screening for malignancies</Text>
+					<Text style={{ width: 350, fontFamily: 'KAIU' }}>檢測項目 : Mutation screening for MPN</Text>
 				</View>
 				
 				<Text style={{fontFamily: 'Times-Bold'}}>{'\n'}I. Variant list:</Text>
